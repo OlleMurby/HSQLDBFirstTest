6eba46935c48472502f47e2192e063afa59c117f
Olle Murby <olle.murby@outlook.com>
2024-03-05 17:53:44 +0100

    Some comments added to HSQLDBFirstTest.java to prepare for next developments (and updated the ID-field name as I now created a different version of the PLAYER table..)

--- src/main/java/omub/hsqldbfirsttest/HSQLDBFirstTest.java
+++ src/main/java/omub/hsqldbfirsttest/HSQLDBFirstTest.java
@@ -19,6 +19,12 @@ public class HSQLDBFirstTest {
 
     public static void main(String[] args) {
         System.out.println("HSQLDBFirstTest: Olle's first crawling steps into database connections from java. This one into HSQLDB");
+        System.out.println("Note: if there is no database available or up and running, and/or if such database does not have a");
+        System.out.println("PLAYERS table created in it, this program will fail hard...");
+        //The Create Table comand is: "CREATE TABLE PLAYER (AUID INTEGER PRIMARY KEY, NAME VARCHAR(255), PASSWORD VARCHAR(50)) ;"
+        //If you need to create the database and table from scracth, start the DatabasemanagerSwing program. Remember to start it from the 
+        // root folder of "this project". Connect to the TestDB02 database using the "file" type, user: "SA" and password: "password"
+        // Then create the new table...
                 
         String db = "" ;
         String user = ""; 
@@ -61,19 +67,20 @@ public class HSQLDBFirstTest {
             nextID = getNextIDAsInt( conn ) ; 
             str_nextID = String.format("%03d", nextID) ;
             
+            //TODO: Change to using parameterised query, as a learning excersice
             insertQuery = "INSERT INTO PLAYER VALUES (" + nextID + " ,'" + RandomNames.rnd_lname() +"','password" + str_nextID + "')";
             stmt = conn.createStatement();
             stmt.execute(insertQuery);
             
             stmt.close();
             conn.close();
+            System.out.println("HSQLDBFirstTest: Have now inserted a row into the PLAYER table, using this SQL command: '" + insertQuery + "'." ) ;
         } catch (SQLException ex) {
             Logger.getLogger(HSQLDBFirstTest.class.getName()).log(Level.SEVERE, null, ex);
             // If you get here from a failed connection, maybe it is because your HSQLDB Server is not running.. see comment above
             // the statement "	db = "jdbc:hsqldb:hsql://localhost/TestDB02" ;"
         }
         
-        System.out.println("HSQLDBFirstTest: Have now inserted a row into the PLAYER table, using this SQL command: '" + insertQuery + "'." ) ;
         System.out.println("HSQLDBFirstTest: Program exited normally.");
         
     } // End of method main
@@ -85,7 +92,7 @@ public class HSQLDBFirstTest {
         
         try {
             Statement loc_stmt= conn.createStatement() ;
-            ResultSet loc_rs = loc_stmt.executeQuery("SELECT MAX(ID) AS MAX_ID FROM PLAYER;") ;
+            ResultSet loc_rs = loc_stmt.executeQuery("SELECT MAX(AUID) AS MAX_ID FROM PLAYER;") ;
 
             while ( loc_rs.next() ) {
                 loc_strNextID = loc_rs.getString("MAX_ID") ;
